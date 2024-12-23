0f35972d9ee414dae69269e3b9fb0abe5343cfee
Dmytro Zubariev <dzu@ciklum.com>
2024-12-23 14:10:13 +0200

    fix

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -7,7 +7,7 @@ export default function SignIn({ providers }) {
       <h1>Sign In</h1>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
-          <button onClick={() => signIn(provider.id, { callbackUrl: 'https://authentication-module-eight.vercel.app/protected' })}>Sign in with {provider.name}</button>
+          <button onClick={() => signIn(provider.id, { callbackUrl: 'https://authentication-module-ke6vx6nt3-dmytros-projects-2a79fa69.vercel.app/protected' })}>Sign in with {provider.name}</button>
         </div>
       ))}
     </div>
