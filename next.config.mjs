23177a316b6c0ef697d0f1108dae952d6b11dc00
Dmytro Zubariev <dzu@ciklum.com>
2024-12-24 14:21:48 +0200

    fix csp

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { createSecureHeaders } from "next-secure-headers";
+
+const nextConfig = {
+  async headers() {
+    return [{
+      source: "/(.*)",
+      headers: createSecureHeaders({
+        contentSecurityPolicy: {
+          directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "accounts.google.com"],
+            connectSrc: ["'self'", "accounts.google.com"],
+            imgSrc: ["'self'", "lh3.googleusercontent.com"],
+          },
+        },
+        forceHTTPSRedirect: [true, { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true }],
+        referrerPolicy: "same-origin",
+      }),
+    }];
+  },
+};;
 
 export default nextConfig;
